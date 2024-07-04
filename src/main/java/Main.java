0e285431fff63217c6b43378d7408f81070e6f1e
ddudalves <169068338+ddudalves@users.noreply.github.com>
2024-07-04 10:56:56 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Digite seu nome:");
+    String nome = sc.next();
+    System.out.println("Digite seu sobrenome:");
+    String sobrenome = sc.next();
+    System.out.println("Digite sua idade:");
+    int idade = sc.nextInt();
+    System.out.println("Digite sua altura:");
+    Double altura = sc.nextDouble();
+    System.out.println("Digite seu sexo:");
+    String sexo = sc.next();
+
+    System.out.println("Olá, " + nome + " " + sobrenome + "! Fizemos uma breve análise das informações passadas e temos as seguintes informações a repassar.");
+sc.close();
+    System.out.println("O seu peso ideal é
+  }
+  
+
+
+}
